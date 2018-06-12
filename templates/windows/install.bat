661f76b18f37c7734e05e5ed56d850408e05dbff
Vladislav <Kasp42@users.noreply.github.com>
2018-06-12 10:02:18 +0300

    Update install.bat
    Add start plink.exe
    Move copy config SVN.

--- templates/windows/install.bat
+++ templates/windows/install.bat
@@ -1,6 +1,6 @@
 @echo off
 net session >nul 2>&1
-if %errorLevel% neq 0 (
+if %ERRORLEVEL% neq 0 (
   color 04
   echo Failure: Current permissions inadequate. Please run this file as admin.
   pause
@@ -30,12 +30,19 @@ echo Creating Symlinks...
 @setlocal enableextensions
 @cd /d "%~dp0"
 
+rem Move config for SVN to %AppData%
+mkdir "%AppData%/Subversion"
+xcopy /E /Y Subversion "%AppData%"
+rd /S /Q Subversion
+
+rem Create symlinks for trunk
 mklink /d .\wl.trunk\core ..\checkout\core\trunk
 mklink /d .\wl.trunk\namespace.Core ..\checkout\namespace\Core\trunk
 mklink /d .\wl.trunk\namespace.Social ..\checkout\namespace\Social\trunk
 mklink /d .\wl.trunk\namespace.Wl ..\checkout\namespace\Wl\trunk
 mklink /d .\wl.trunk\project ..\checkout\reservationspot.com\trunk\
 
+rem Create symlinks for stable
 mklink /d .\wl.stable\core ..\checkout\core\servers\stable.wellnessliving.com
 mklink /d .\wl.stable\namespace.Core ..\checkout\namespace\Core\servers\wl-stable
 mklink /d .\wl.stable\namespace.Social ..\checkout\namespace\Social\servers\wl-stable
@@ -60,10 +67,6 @@ if %ERRORLEVEL% neq 0 (
   @echo %NEWLINE%^127.0.0.1 %HOST_STABLE%>>%WINDIR%\System32\drivers\etc\hosts
 )
 
-mkdir "%AppData%/Subversion"
-xcopy /E /Y Subversion "%AppData%"
-rd /S /Q Subversion
-
 :SUBVERSION
 start /wait checkout/reservationspot.com/install/templates/windows/Setup-Subversion-1.7.9.msi
 if %ERRORLEVEL% equ 1063 (
@@ -80,6 +83,11 @@ if %ERRORLEVEL% equ 1063 (
 )
 
 :PUTTY
+color 06
+echo When installing PuTTY, do not change the installation path.
+color
+pause
+
 start /wait checkout/reservationspot.com/install/templates/windows/putty-64bit-0.70-installer.msi
 if %ERRORLEVEL% neq 0 (
   if %ERRORLEVEL% equ 1602 (
@@ -97,6 +105,13 @@ if %ERRORLEVEL% neq 0 (
 
 color 02
 echo Symlinks created.
+
+rem Go to folder with Putty and start plink.exe for save key.
+echo When you see:
+echo ( success ( 2 2 ( ) ( edit-pipeline svndiff1 absent-entries commit-revprops depth log-revprops atomic-revprops partial-replay inherited-props ephemeral-txnprops file-revs-reverse ) ) )
+echo Click Ctrl+C
+cd "%ProgramFiles%\PuTTY\"
+plink.exe -P 35469 -l svn -I %workspace%\keys\libs.ppk libs.svn.1024.info
 
 pause
 del "%~f0"
