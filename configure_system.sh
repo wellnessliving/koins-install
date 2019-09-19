66e8571887107f54ea598e04f17193464335e642
Vladislav <Kasp42@users.noreply.github.com>
2019-09-19 13:08:38 +0300

    Update configure_system.sh

--- configure_system.sh
+++ configure_system.sh
@@ -5,3 +5,6 @@ crudini --set /etc/mysql/my.cnf mysqld sql_mode ""
 crudini --set /etc/mysql/my.cnf mysqld character_set_server "binary"
 crudini --set /etc/mysql/my.cnf mysqld log_bin_trust_function_creators "ON"
 crudini --set /etc/mysql/my.cnf mysqld max_allowed_packet "104857600"
+
+service mysql restart
+service apache2 restart
