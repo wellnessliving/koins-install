a317111592a5cd5d86b503c5ca6b9f2a55be645c
Vladislav <Kasp42@users.noreply.github.com>
2019-09-19 15:32:29 +0300

    Update configure_system.sh

--- configure_system.sh
+++ configure_system.sh
@@ -4,11 +4,11 @@ mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u root -p mysql
 
 # Configuring mysql
 cp /etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf.tmp
-sed -i 's/skip\-external\-locking/skip-external-locking=/g' /etc/mysql/mysql.conf.d/mysqld.cnf
-crudini --set /etc/mysql/mysql.conf.d/mysqld.cnf sql_mode ""
-crudini --set /etc/mysql/mysql.conf.d/mysqld.cnf character_set_server "binary"
-crudini --set /etc/mysql/mysql.conf.d/mysqld.cnf log_bin_trust_function_creators "ON"
-crudini --set /etc/mysql/mysql.conf.d/mysqld.cnf max_allowed_packet "104857600"
+sed -i 's/skip\-external\-locking/skip-external-locking=/g' mysqld /etc/mysql/mysql.conf.d/mysqld.cnf
+crudini --set /etc/mysql/mysql.conf.d/mysqld.cnf mysqld sql_mode ""
+crudini --set /etc/mysql/mysql.conf.d/mysqld.cnf mysqld character_set_server "binary"
+crudini --set /etc/mysql/mysql.conf.d/mysqld.cnf mysqld log_bin_trust_function_creators "ON"
+crudini --set /etc/mysql/mysql.conf.d/mysqld.cnf mysqld max_allowed_packet "104857600"
 
 # Configuring PHP
 crudini --set /etc/php/7.2/apache2/php.ini PHP allow_url_fopen "1"
