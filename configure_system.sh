505b6c210807d2db47db9a6d446ef680fe9013d1
Vladislav <Kasp42@users.noreply.github.com>
2019-09-19 13:07:42 +0300

    Create configure_system.sh

---
+++ configure_system.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u root -p mysql
+crudini --set /etc/mysql/my.cnf mysqld sql_mode ""
+crudini --set /etc/mysql/my.cnf mysqld character_set_server "binary"
+crudini --set /etc/mysql/my.cnf mysqld log_bin_trust_function_creators "ON"
+crudini --set /etc/mysql/my.cnf mysqld max_allowed_packet "104857600"
