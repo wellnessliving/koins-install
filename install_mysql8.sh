dee939bad8f7ebe029005f6bb00ec073a75632c7
Vladislav <Kasp42@users.noreply.github.com>
2020-04-08 15:39:58 +0300

    Update and rename mysql5_7to8_0_16.sh to install_mysql8.sh

--- install_mysql8.sh
+++ install_mysql8.sh
@@ -9,7 +9,7 @@ if [[ "x$(id -u)" != 'x0' ]]; then
 fi
 
 # Asking for confirmation to proceed
-echo "This script remove MySql 5.7 and install MySql 8.0.16."
+echo "This script remove MySql 5.7 and install MySql 8"
 echo "All databases will be deleted!!!"
 read -p 'Would you like to continue [y/n]: ' answer
 if [[ "$answer" != 'y' ]] && [[ "$answer" != 'Y'  ]]; then
@@ -24,16 +24,11 @@ apt install libaio1 libaio-dev -y
 
 package_list=$(mktemp -p /tmp)
 dpkg --get-selections > ${package_list}
-if [[ -z "$(grep mysql-server* ${package_list})" ]]; then
-  echo "MySql is not installed."
-  exit 1
-fi
-
-dpkg --get-selections > ${package_list}
 if [[ -z "$(grep crudini ${package_list})" ]]; then
   echo "Crudini is not installed."
   exit 1
 fi
+rm ${package_list}
 
 service mysql stop
 
@@ -42,18 +37,16 @@ apt purge mysql-server* mysql-client* mysql-common* -y
 apt autoremove -y
 apt autoclean -y
 
-service mysql status
-if [[ $? -eq 0 ]]; then
-  echo "MySql service is exist. Maybe MySql is not deleting"
-  exit 1
-fi
-
 rm -rf /etc/mysql
 rm -rf /usr/local/mysql
 rm -rf /usr/local/sql
 
 # Download MySql 8.0.16 sources
 wget -c https://downloads.mysql.com/archives/get/p/23/file/mysql-8.0.16-linux-glibc2.12-x86_64.tar.xz
+if [[ "$?" -gt 0 ]]; then
+  echo "Cannot download mysql"
+  exit 1
+fi
 
 # Extract all files from archive and delete archive.
 mkdir -p /usr/local/sql
@@ -63,7 +56,7 @@ rm -rf mysql-8.0.16-linux-glibc2.12-x86_64.tar.xz
 # Installing MySql
 SQL_BIN="mysql-8.0.16-linux-glibc2.12-x86_64"
 
-groupadd mysql
+groupadd -f mysql
 useradd -r -g mysql -s /bin/false mysql
 cd /usr/local
 ln -s /usr/local/sql/${SQL_BIN} /usr/local/mysql
@@ -74,7 +67,12 @@ bin/mysqld --initialize-insecure --basedir=/usr/local/mysql --datadir=/usr/local
 chown mysql:mysql -R /usr/local/sql/${SQL_BIN}
 
 # Creating mysql service and command
-export PATH=$PATH:/usr/local/mysql/bin
+for s_bin in /usr/local/mysql/bin/*; do
+  s_file=$(basename ${s_bin})
+  if [[ ${s_file} == *"mysql"* ]]; then
+    ln -s ${s_bin} /usr/bin/${s_file}
+  fi
+done
 
 ln -s /usr/local/mysql/support-files/mysql.server /etc/init.d/mysql
 
@@ -98,17 +96,5 @@ service mysql start
 mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u root mysql
 crudini --set /etc/mysql/my.cnf mysqld default_time_zone "UTC"
 
-touch /root/.bash_profile
-
-echo "# .bash_profile" > /root/.bash_profile
-echo "" >> /root/.bash_profile
-echo "# Get the aliases and functions" >> /root/.bash_profile
-echo "if [ -f ~/.bashrc ]; then" >> /root/.bash_profile
-echo "    . ~/.bashrc" >> /root/.bash_profile
-echo "fi" >> /root/.bash_profile
-echo "" >> /root/.bash_profile
-echo "# User specific environment and startup programs" >> /root/.bash_profile
-echo "" >> /root/.bash_profile
-echo "PATH=\$PATH:\$HOME/bin:/usr/bin:/usr/local/mysql/bin" >> /root/.bash_profile
-
 service mysql restart
+
