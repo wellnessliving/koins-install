94b252b122d1e72152e1e2a35404de32b09808a0
Vladislav Kobzev <Kasp42@users.noreply.github.com>
2020-07-24 14:07:26 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -353,6 +353,9 @@ apt-get purge php7.3-cli php7.4-cli -y
 apt -y install php7.2-dev php-pear
 pecl install sync
 
+# Install Gearman
+apt -y install gearman php-gearman
+
 echo -e "${Purple}#----------------------------------------------------------#
 #                    Configuring system                    #
 #----------------------------------------------------------#${NC}"
@@ -512,7 +515,7 @@ crudini --set /etc/mysql/my.cnf mysqld default_authentication_plugin "mysql_nati
 crudini --set /etc/mysql/my.cnf mysqld innodb_use_native_aio "off"
 crudini --set /etc/mysql/my.cnf mysqld log_error "/var/log/mysql/mysql.log"
 
-service mysql restart
+service mysql start
 
 # Load timezone to mysql
 mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u root mysql
@@ -887,6 +890,7 @@ update-rc.d memcached defaults
 service apache2 restart
 service mysql restart
 service memcached restart
+service gearman-job-serve start
 
 echo -e "${Green}
 Installation finished successfully.
